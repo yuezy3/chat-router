704cc102009e83defe09dca1f1a46ff0261de379
zeyang yue <yuezy3@163.com>
2023-04-08 06:22:21 +0000

    add chat api

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World! test updating');
+})
+
+// for chat
+app.post("/chatapi",(req, res)=>{
+    let data = req.body;
+    console.log(data);
+    let resdata = {
+        "code": 0,
+        "message": "string",
+        "data": {
+            "type": 0,
+            "info": {
+                "text": "string"
+            }
+        }
+    };
+    res.send(JSON.stringify(resdata));
 })
 
 app.listen(port, () => {
